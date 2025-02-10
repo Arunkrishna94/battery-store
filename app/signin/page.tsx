dfdfcad248d37be4a5b039074de8dbea7d9dd193
ArunKrisha94 <arunkrishna94@outlook.com>
2025-02-10 18:36:19 +0000

    feature

---
+++ app/signin/page.tsx
@@ -0,0 +1,10 @@
+// app/signin/page.tsx
+export default function SignIn() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Sign In</h1>
+        <p>Sign in functionality will be added here.</p>
+      </div>
+    );
+  }
+  
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+// app/signup/page.tsx
+export default function SignUp() {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">Sign Up</h1>
+        <p>Sign up functionality will be added here.</p>
+      </div>
+    );
+  }
+  
